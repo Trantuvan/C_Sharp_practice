a09f1c3741fc1648550e3ef7034dacad4518a079
Trần Văn <Trantuvan.kan@gmail.com>
2021-05-22 19:23:42 +0700

    new lesson from intermediate mosh

---
+++ Indexers/Indexers/HttpCookie.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Indexers
+{
+    public class HttpCookie
+    {
+        //declaring a dict and always remember to initialize with empty dict avoid null excpetion
+        //adding readonly modifier to avoid lost data
+        private readonly Dictionary<string, string> _dictionary;
+
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        //declaring an indexers for dictionary
+        public string this[string key]
+        { 
+            get 
+            { /* return the specified index here */
+                return _dictionary[key];
+            }
+            set 
+            { /* set the specified index to value here */
+                _dictionary[key] = value;
+            }
+        }
+    }
+}
---
+++ Indexers/Indexers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //indexers is a properties using when a class has a list or dictionary
+            var cookie = new HttpCookie();
+            cookie["name"] = "Van";
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
